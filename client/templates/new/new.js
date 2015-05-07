be4cf065b66a26c094cc69c907b819c68472bd79
Ilia <ilia.reingold@gmail.com>
2015-05-07 14:54:13 -0400

    form error checking works like a charm

--- client/templates/new/new.js
+++ client/templates/new/new.js
@@ -10,43 +10,24 @@ var counter = 1;
 Template.new.events({
   'click [data-add-factor]': function() {
 
-    // visually change the remove factor button if there are no factor fields
+    // lets validate that title/first/second option are all set
+    var title = $('input[name="title"]').val(),
+        firstoption = $('input[name="firstoption"]').val(),
+        secondoption = $('input[name="secondoption"]').val();
 
-    if ($('button[data-remove-factor]').hasClass('disabled')) {
-      $('button[data-remove-factor]').removeClass('disabled');  
-    }
-
-    // append a new factor field with each click of +factor, and animate it in
-    $('.ui.form')  
-    .append('<div style="left: 500px; opacity: 0;" class="two factor fields ui segment"><div class="field"><label> Factor </label> <input type="text" name="factor' + counter + '"> </div> <div class="field"> <label> Weighting </label> <input type="text" name="weight' + counter + '"> </div> </div> ');      
-
-    var odd = $('div.two.factor.fields').length;
-    var target = $('div.two.factor.fields').last();
-
-    if (odd % 2 == 0) {
-      target.addClass('pink')
+    if (title && firstoption && secondoption) {
+      executeEvents();
+      clearFormErrors();
     } else {
-      target.addClass('teal')
-      target.css("left", "-500px");
-    }
-
-    $('div.two.factor.fields').last().velocity({ opacity: 1, left: "0px" }, { 
-      duration: "slow",
-      begin: function() {
-        // let's scroll to the newly created element
-        target.get(0).scrollIntoView({block: "end", behavior: "smooth"});
-      }
-    });
-
-    // increment the counter everytime we add a field
-    counter++;
+      showFormErrors();
+    };
   },
   'click [data-remove-factor]': function( e, tpl ){
 
     // look in the form for the last factor-field, animate it out, and remove it from the dom
 
     var form = $('.ui.form');  
-    var last = form.children('div.two.factor.fields').last();
+    var last = form.children('.ui.segment').last();
     last.velocity({ 
       opacity: 0 
     }, { 
@@ -56,7 +37,7 @@ Template.new.events({
 
         // reset the remove factor button if the last field has been removed
 
-        if ( form.children('div.two.factor.fields').length == 0 ) {
+        if ( form.children('.ui.segment').length == 0 ) {
           $('button[data-remove-factor]').addClass('disabled');  
         }
 
@@ -66,21 +47,21 @@ Template.new.events({
     });
   },
   'click [data-submit-decision]': function( e, tpl ) {
-    
+
     // loop through all the factors to generate an object
     
-    var factors = $('div.two.factor.fields'),
-        size = factors.length,
-        decision = {};
-        decision.owner = Meteor.userId();
-        decision.title = $('input[name="title"]').val();
-        decision.createdOn = moment().format("MMM Do YYYY");
+    var factors = $('.ui.segment'),
+    size = factors.length,
+    decision = {};
+    decision.owner = Meteor.userId();
+    decision.title = $('input[name="title"]').val();
+    decision.createdOn = moment().format("MMM Do YYYY");
 
         // do we need a field which tracks whether a decision was weighed?
 
         // decision.concluded = false;
 
-    $.each(factors, function( index, item ){
+        $.each(factors, function( index, item ){
 
       // make sure index starts at 1
 
@@ -98,13 +79,60 @@ Template.new.events({
 
     });
 
-    Decisions.insert(decision, function( err, id ){
-      if (!err) {
-        Router.go("/");  
-      }
-      
-      // TODO sexy vex alert confirming submission, or showing errors
-      // then redirecting the user to actually making the decision
-    });
-  }
+      Decisions.insert(decision, function( err, id ){
+        if (!err) {
+          Router.go("/");  
+        }
+      });
+    }
 })
+
+function showFormErrors() {
+  var error = $('#formError');
+  if (error.length == 0) {
+    $('.twelve.wide.column').append('<div id="formError" class="ui red message">Please enter a Title, First Option & Second Option</div>')
+  } 
+}
+
+function clearFormErrors() {
+   // remove warning message if it exists
+    var error = $('#formError');
+    if (error) {
+      error.remove();
+    }
+}
+
+function executeEvents() {
+   
+
+    // visually change the remove factor button if there are no factor fields
+
+    if ($('button[data-remove-factor]').hasClass('disabled')) {
+      $('button[data-remove-factor]').removeClass('disabled');  
+    }
+
+    // append a new factor field with each click of +factor, and animate it in
+    $('.ui.form')  
+    .append('<div style="left: 500px; opacity: 0;" class="ui segment"> <div class="two factor fields"> <div class="field"> <label> Factor </label> <input type="text" name="factor' + counter + '"> </div> <div class="field"> <label> Weighting </label> <input type="text" name="weight' + counter + '"> </div> </div> <div class="two factor fields"> <div class="field"> <label> Option 1 Points </label> <input type="text" name="factor' + counter + '"> </div> <div class="field"> <label> Option 2 Points </label> <input type="text" name="weight' + counter + '"> </div> </div> </div>');      
+
+    var odd = $('.ui.segment').length;
+    var target = $('.ui.segment').last();
+
+    if (odd % 2 == 0) {
+      target.addClass('pink')
+    } else {
+      target.addClass('teal')
+      target.css("left", "-500px");
+    }
+
+    $('.ui.segment').last().velocity({ opacity: 1, left: "0px" }, { 
+      duration: "slow",
+      begin: function() {
+        // let's scroll to the newly created element
+        target.get(0).scrollIntoView({block: "end", behavior: "smooth"});
+      }
+    });
+
+    // increment the counter everytime we add a field
+    counter++; 
+}
